3b0af2ea1377ab7d20593cd23ac3a37b5f96c94f
Student <Student@CAB4-10>
2023-05-17 08:24:58 +1000

    Добавьте файлы проекта.

---
+++ schedule/Replacement.xaml.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using System.Data.SqlClient;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+using System.Windows.Markup;
+using System.Text.RegularExpressions;
+
+namespace schedule
+{
+    /// <summary>
+    /// Логика взаимодействия для Replacement.xaml
+    /// </summary>
+    public partial class Replacement : Window, INotifyPropertyChanged
+    {
+        public ObservableCollection<TblReplacement> tReplacements { get; set; } = new ObservableCollection<TblReplacement>();
+        public ObservableCollection<Schedule> schedules { get; set; } = new ObservableCollection<Schedule>();
+
+
+        //public List<TblReplacement> tblRepl { get; set; }
+
+        public List<TblReplacement> Tbl_replacement { get; set; }
+        public DbSet<TblReplacement> tblReplacements { get; set; }
+
+
+        //не нужное
+        public CustomCommand SimpleCommand { get; set; }
+        public CustomCommand TipleCommand { get; set; }//сохранить
+        public CustomCommand PipleCommand { get; set; }//добавить
+        public CustomCommand FlipCommand { get; set; }
+
+
+
+        //нужное
+        public CustomCommand GipleCommand { get; set; }//удалить
+        
+
+
+
+
+        public Schedule SelectedSavod { get; set; }
+        public Schedule ChangeSavod { get; set; }
+        public Schedule SelectedSchedule { get; set; }
+        public Schedule ChangeSchedule { get; set; }
+
+
+        private Visibility visibility = Visibility.Hidden;
+        public Visibility kreating
+        {
+            get
+            {
+                return visibility;
+            }
+            set
+            {
+                visibility = value;
+
+                Fill("kreating");
+            }
+        }
+
+       
+
+
+        public Schedule schedule { get => selected; set { selected = value; Fill(); } }
+        public Schedule Sel { get => sel; set { sel = value; Fill(); } }
+
+
+        private Schedule selected = new Schedule();
+       
+        public TblReplacement Selected { get => selected1; set { selected1 = value; Fill(); } }
+        private TblReplacement selected1;
+
+        private Schedule sel = new Schedule();
+
+      
+        private string search;
+
+
+
+
+
+
+        //Поиск
+        void Signal(string prop) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        private string searchText = "";
+
+
+        public string SearchText
+        {
+            get => searchText;
+            set
+            {
+                searchText = value;
+                Search();
+            }
+        }
+        private void Search()
+        {
+            var db = new ScheduleDbContext();
+            var result = db.TblReplacements.Where(s =>
+                    s.Name.Contains(searchText) /*||*/
+                );
+            tbl_replacement = result.ToList();
+            Signal(nameof(tbl_replacement));
+
+        }
+
+
+
+        public List<TblWeekday> Day { get; set; }
+
+        public TblWeekday SelectedDay
+        {
+            get => SelectedDay1; set
+            {
+                SelectedDay1 = value;
+                var db = new ScheduleDbContext();
+                tbl_replacement = db.TblReplacements.Where(s => s.WeekdaysId == SelectedDay.Id).ToList();
+            }
+        }
+
+        public TblReplacement replacement { get => replacement1; set { replacement1 = value; Fill(); } }
+        private TblReplacement replacement1 = new TblReplacement();
+
+        public List<TblReplacement> tbl_replacement { get => tbl_replacement1; set { tbl_replacement1 = value; Fill(); } }
+        private List<TblReplacement> tbl_replacement1;
+
+        private TblWeekday SelectedDay1;
+        public List<TblReplacement> TblReplacement { get => TblReplacement1; set { TblReplacement1 = value; Fill(); } }
+        private List<TblReplacement> TblReplacement1;
+
+        public Replacement()
+        {
+            InitializeComponent();
+            DataContext = this;
+            //поиск
+            Search();
+
+
+
+
+            //показывает данные дни недели
+            Day = DB.GetInstance().TblWeekdays.ToList();
+
+            TblReplacement = DB.GetInstance().TblReplacements.ToList();
+            tbl_replacement = DB.GetInstance().TblReplacements.ToList();
+           
+            //скрывает все данные в List View tbl_replacement
+            //tbl_replacement = DB.GetInstance().TblReplacements.Where(s => s.Equals(SelectedDay)).ToList();
+            Combobox3.ItemsSource = Day;
+
+            //tbl2 = DB.GetInstance().TblScheduleDbs.Where(s => s.Groupid == grud.GroupId).ToList();
+
+            GipleCommand = new CustomCommand(
+               () => { kreating = Visibility.Visible; });
+        }
+
+
+
+        private void txtFilter_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(listschedule.ItemsSource).Refresh();
+        }
+
+        private void Fill([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void ButtonBack(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        public delegate void RoutedEventHandler(object sender, RoutedEventArgs e);
+        private void Dob(object sender, RoutedEventArgs e)
+        {
+            if(SelectedDay == null)
+            {
+                MessageBox.Show("Выберите день недели", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            //var result = MessageBox.Show("Хотите выполнить действие?",
+            //    "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            else 
+            {
+                MessageBox.Show("Запись добавлена в БД", "Готово!", MessageBoxButton.OK,
+                    MessageBoxImage.Asterisk);
+                using (var db = new ScheduleDbContext())
+                {
+                    kreating = Visibility.Visible;
+                    Fill(nameof(tReplacements));
+
+
+
+                    replacement.WeekdaysId = SelectedDay.Id;
+                    db.TblReplacements.Add(replacement);
+                 
+
+                    db.SaveChanges();
+                    tbl_replacement = DB.GetInstance().TblReplacements.Where(s => s.WeekdaysId == SelectedDay.Id).ToList();
+                    //tbl_replacement = db.TblReplacements.Where(s => s.Id == SelectedDay.Id).ToList();
+
+                    //DB.GetInstance().TblReplacements.Update(Selected);
+                    ////tbl_replacement = DB.GetInstance().TblReplacements.ToList();
+                    replacement = new TblReplacement();
+
+                }
+            }
+           
+        }
+
+
+        private void nasad(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Delet(object sender, RoutedEventArgs e)
+        {
+            //if (Selected == null)
+            //{
+            //    MessageBox.Show("Ошибка, нельзя удалить!", "Ошибка 404", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+
+          
+            var result = MessageBox.Show("Хотите удалить запись?", "Подтверждение",
+                MessageBoxButton.YesNo, MessageBoxImage.Question); 
+
+            if (result == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Запись удалена из БД", "Готово!", MessageBoxButton.OK,
+                    MessageBoxImage.Asterisk);
+                using (var db = new ScheduleDbContext())
+                {
+
+                    //удаляет по id в combobox
+                    var remove = DB.GetInstance().TblReplacements.Find(Selected.Id);
+                    DB.GetInstance().TblReplacements.Remove(remove);
+                    DB.GetInstance().SaveChanges();
+                    tbl_replacement = DB.GetInstance().TblReplacements.Where(s => s.WeekdaysId == SelectedDay.Id).ToList();
+
+                    //tbl_replacement = DB.GetInstance().TblReplacements.ToList();
+                }
+            }
+        }
+
+        private void saved(object sender, RoutedEventArgs e)
+        {
+            DB.GetInstance().SaveChanges();
+        }
+    }
+}
---
+++ schedule/ScheduleDbContext.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace schedule;
+
+public partial class ScheduleDbContext : DbContext
+{
+    public ScheduleDbContext()
+    {
+    }
+
+    public ScheduleDbContext(DbContextOptions<ScheduleDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Pair1> Pair1s { get; set; }
+
+    public virtual DbSet<TblCourse> TblCourses { get; set; }
+
+    public virtual DbSet<TblDay> TblDays { get; set; }
+
+    public virtual DbSet<TblGroup> TblGroups { get; set; }
+
+    public virtual DbSet<TblPair> TblPairs { get; set; }
+
+    public virtual DbSet<TblPredmet> TblPredmets { get; set; }
+
+    public virtual DbSet<TblReplacement> TblReplacements { get; set; }
+
+    public virtual DbSet<TblScheduleDb> TblScheduleDbs { get; set; }
+
+    public virtual DbSet<TblSemester> TblSemesters { get; set; }
+
+    public virtual DbSet<TblSemestr> TblSemestrs { get; set; }
+
+    public virtual DbSet<TblSpeciality> TblSpecialities { get; set; }
+
+    public virtual DbSet<TblWeekday> TblWeekdays { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("host=192.168.200.13;user=student;password=student;database=schedule_db", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.3.38-mariadb"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_general_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Pair1>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("pair1");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+        });
+
+        modelBuilder.Entity<TblCourse>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("tbl_course");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.Course).HasColumnType("int(11)");
+        });
+
+        modelBuilder.Entity<TblDay>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("tbl_day");
+
+            entity.HasIndex(e => e.PairId, "FK_tbl_day_tbl_pairs_Id");
+
+            entity.Property(e => e.Id).HasColumnType("int(11)");
+            entity.Property(e => e.DaySid)
+                .HasColumnType("int(11)")
+                .HasColumnName("DaySId");
+            entity.Property(e => e.PairId).HasColumnType("int(11)");
+
+            entity.HasOne(d => d.Pair).WithMany(p => p.TblDays)
+                .HasForeignKey(d => d.PairId)
+                .HasConstraintName("FK_tbl_day_tbl_pairs_Id");
+        });
+
+        modelBuilder.Entity<TblGroup>(entity =>
+        {
+            entity.HasKey(e => e.GroupId).HasName("PRIMARY");
+
+            entity.ToTable("tbl_group");
+
+            entity.HasIndex(e => e.SemestrNuberId, "FK_tbl_group");
+
+            entity.HasIndex(e => e.CourseId, "FK_tbl_group_tbl_course_id");
+
+            entity.HasIndex(e => e.SemestrWeekId, "FK_tbl_group_tbl_semestr_id");
+
+            entity.HasIndex(e => e.SpecialityId, "FK_tbl_group_tbl_speciality_specialityID");
+
+            entity.Property(e => e.GroupId)
+                .HasColumnType("int(11)")
+                .HasColumnName("GroupID");
+            entity.Property(e => e.CourseId)
+                .HasColumnType("int(11)")
+                .HasColumnName("courseID");
+            entity.Property(e => e.Group).HasColumnType("int(11)");
+            entity.Property(e => e.SemestrNuberId)
+                .HasColumnType("int(11)")
+                .HasColumnName("semestrNuberID");
+            entity.Property(e => e.SemestrWeekId)
+                .HasColumnType("int(11)")
+                .HasColumnName("semestrWeekID");
+            entity.Property(e => e.SpecialityId)
+                .HasColumnType("int(11)")
+                .HasColumnName("specialityID");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.TblGroups)
+                .HasForeignKey(d => d.CourseId)
+                .HasConstraintName("FK_tbl_group_tbl_course_id");
+
+            entity.HasOne(d => d.SemestrNuber).WithMany(p => p.TblGroupSemestrNubers)
+                .HasForeignKey(d => d.SemestrNuberId)
+                .HasConstraintName("FK_tbl_group");
+
+            entity.HasOne(d => d.SemestrWeek).WithMany(p => p.TblGroupSemestrWeeks)
+                .HasForeignKey(d => d.SemestrWeekId)
+                .HasConstraintName("FK_tbl_group_tbl_semestr_id");
+
+            entity.HasOne(d => d.Speciality).WithMany(p => p.TblGroups).HasForeignKey(d => d.SpecialityId);
+        });
+
+        modelBuilder.Entity<TblPair>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("tbl_pairs");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnType("int(11)");
+            entity.Property(e => e.Pairid).HasColumnType("int(11)");
+        });
+
+        modelBuilder.Entity<TblPredmet>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("tbl_predmet");
+
+            entity.HasIndex(e => e.Factionid, "FK_tbl_predmet_tbl_faction_id");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.Factionid)
+                .HasColumnType("int(11)")
+                .HasColumnName("factionid");
+            entity.Property(e => e.Predmet)
+                .HasMaxLength(255)
+                .HasColumnName("predmet");
+        });
+
+        modelBuilder.Entity<TblReplacement>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("tbl_replacement");
+
+            entity.HasIndex(e => e.WeekdaysId, "FK_tbl_replacement");
+
+            entity.Property(e => e.Id).HasColumnType("int(11)");
+            entity.Property(e => e.Cabinet).HasColumnType("int(11)");
+            entity.Property(e => e.Group).HasColumnType("int(11)");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasDefaultValueSql("''");
+            entity.Property(e => e.Pair).HasColumnType("int(11)");
+            entity.Property(e => e.Predmet)
+                .HasMaxLength(255)
+                .HasDefaultValueSql("''");
+            entity.Property(e => e.WeekdaysId)
+                .HasColumnType("int(11)")
+                .HasColumnName("weekdaysId");
+
+            entity.HasOne(d => d.Weekdays).WithMany(p => p.TblReplacements)
+                .HasForeignKey(d => d.WeekdaysId)
+                .HasConstraintName("FK_tbl_replacement");
+        });
+
+        modelBuilder.Entity<TblScheduleDb>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("tbl_schedule_db");
+
+            entity.HasIndex(e => e.Groupid, "FK_tbl_schedule_db_tbl_group_GroupID");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.Cabinet).HasColumnType("int(11)");
+            entity.Property(e => e.Day)
+                .HasMaxLength(255)
+                .HasDefaultValueSql("''");
+            entity.Property(e => e.Groupid).HasColumnType("int(11)");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasDefaultValueSql("''");
+            entity.Property(e => e.Pair).HasColumnType("int(11)");
+            entity.Property(e => e.Predmet)
+                .HasMaxLength(255)
+                .HasDefaultValueSql("''");
+
+            entity.HasOne(d => d.Group).WithMany(p => p.TblScheduleDbs)
+                .HasForeignKey(d => d.Groupid)
+                .HasConstraintName("FK_tbl_schedule_db_tbl_group_GroupID");
+        });
+
+        modelBuilder.Entity<TblSemester>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("tbl_semester");
+
+            entity.HasIndex(e => e.SpecialityId, "FK_tbl_predmetid_tbl_speciality_specialityID");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnType("int(11)")
+                .HasColumnName("ID");
+            entity.Property(e => e.SemesterNumber)
+                .HasColumnType("int(11)")
+                .HasColumnName("semesterNumber");
+            entity.Property(e => e.SemesterWeek)
+                .HasColumnType("int(11)")
+                .HasColumnName("semesterWeek");
+            entity.Property(e => e.SpecialityId)
+                .HasColumnType("int(11)")
+                .HasColumnName("specialityID");
+        });
+
+        modelBuilder.Entity<TblSemestr>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("tbl_semestr");
+
+            entity.HasIndex(e => e.CourseId, "FK_tbl_semestr_tbl_course_id");
+
+            entity.HasIndex(e => e.SemesterNuber, "FK_tbl_semestr_tbl_group_GroupID");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.CourseId)
+                .HasColumnType("int(11)")
+                .HasColumnName("CourseID");
+            entity.Property(e => e.SemesterNuber)
+                .HasColumnType("int(11)")
+                .HasColumnName("semesterNuber");
+            entity.Property(e => e.SemesterWeek)
+                .HasColumnType("int(11)")
+                .HasColumnName("semesterWeek");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.TblSemestrs)
+                .HasForeignKey(d => d.CourseId)
+                .HasConstraintName("FK_tbl_semestr_tbl_course_id");
+        });
+
+        modelBuilder.Entity<TblSpeciality>(entity =>
+        {
+            entity.HasKey(e => e.SpecialityId).HasName("PRIMARY");
+
+            entity.ToTable("tbl_speciality");
+
+            entity.Property(e => e.SpecialityId)
+                .HasColumnType("int(11)")
+                .HasColumnName("specialityID");
+            entity.Property(e => e.Speciality)
+                .HasMaxLength(255)
+                .HasColumnName("speciality");
+        });
+
+        modelBuilder.Entity<TblWeekday>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("tbl_weekdays");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.Day).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
